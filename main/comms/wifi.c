6c006986e5aaac766061f516b575ceb513a23664
Kn0tas <linusekvall89@gmail.com>
2025-12-18 07:38:37 +0100

    fix comilation error

--- main/comms/wifi.c
+++ main/comms/wifi.c
@@ -45,7 +45,11 @@ esp_err_t wifi_init(void) {
     s_wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    ESP_ERROR_CHECK(esp_netif_create_default_wifi_sta());
+    esp_netif_t *sta = esp_netif_create_default_wifi_sta();
+    if (!sta) {
+        ESP_LOGE(TAG, "failed to create default wifi STA netif");
+        return ESP_FAIL;
+    }
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
